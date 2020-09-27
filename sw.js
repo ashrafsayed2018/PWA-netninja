71ab8b1681d628c513c2729f59c2955f2181a66f
ashrafsayed2018 <you@example.com>
2020-09-27 16:16:25 +0300

    the fallback page and the limiting the dynamic caches function

--- sw.js
+++ sw.js
@@ -1,19 +1,37 @@
-const staticCacheName = "static-site";
+const staticCacheName = "static-site-v4";
+const daynamicCacheName   = "site-daynamic-v1";
 const assets = [
     '/',
     '/index.html',
+    '/pages/fallback.html',
     '/js/app.js',
     '/js/ui.js',
     '/js/materialize.min.js',
+    '/manifest.json',
     '/css/styles.css',
     '/css/materialize.min.css',
     '/img/dish.png',
-    'https://fonts.googleapis.com/icon?family=Material+Icons'
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2'
+    
 
 ]
 
+// function to limit the size daynamic cache 
+
+const limitCacheSize = (name, size) => {
+    caches.open(name).then(cache => {
+        cache.keys().then(keys => {
+            // if(keys.length > size) {
+            //     cache.delete(keys[0]).then(limitCacheSize(name,size))
+            // }
+             while(keys.length > size) { cache.delete(keys[0]) }
+        })
+    })
+}
+
 // install the service worker 
- 
+  
 self.addEventListener('install' , event => {
     //  console.log(event)
 
@@ -35,18 +53,43 @@ self.addEventListener('install' , event => {
 
 self.addEventListener('activate' , event => {
     // console.log(event)
+    // delete the old cache version
+    event.waitUntil(
+        caches.keys().then(keys => {
+            // console.log(keys) 
+     
+            return Promise.all(keys.filter(key => key != staticCacheName &&  key != daynamicCacheName)
+            .map(key => caches.delete(key))
+            )
+    
+        })
+    )
 });
 
 
 // fetch event
 
 self.addEventListener('fetch', event => {
-    // console.log('fetch event' , event)
+    // console.log('fetch event' , event) 
 
     event.respondWith(
         caches.match(event.request)
         .then(cacheRes => {
-          return  cacheRes || fetch(event.request)
+          return  cacheRes || fetch(event.request).then(fetchRes => {
+
+              // dsynamic cache the pages which the user visit 
+
+              return caches.open(daynamicCacheName).then(cache => {
+                 cache.put(event.request.url,fetchRes.clone());
+                 limitCacheSize(daynamicCacheName,1)
+                 return fetchRes;
+              });  
+          });
+        }).catch(() => {
+            if(event.request.url.includes('.html')) {
+                
+               return caches.match('/pages/fallback.html')
+            }
         })
-    )
+    );
 })
